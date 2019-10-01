d87b47908798dd1333e4298349ac6d70f9e3b47a
felipe-novillo <55851886+felipe-novillo@users.noreply.github.com>
2019-10-01 13:12:58 -0500

    Add files via upload

---
+++ area circulo.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#define pi 3.1416
+
+
+void main()
+{
+	float radio,area=0;
+	printf("Ingrese el radio del circulo\n");
+	scanf("%f",&radio);
+	area=pi*radio*radio;
+	printf("el area del circulo es: %.2f",area);
+	
+	
+	
+	
+}
+
